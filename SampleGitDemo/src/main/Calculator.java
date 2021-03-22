f1b7f08b9711573eaf1864e3ef20de2966530d85
NandiniUpadhyay-creator <81158473+NandiniUpadhyay-creator@users.noreply.github.com>
2021-03-22 18:37:29 +0530

    added diff function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -3,7 +3,7 @@ package main;
 public class Calculator {
 	
 	public static void main(String []args){
-		System.out.println("Hello World");
+		System.out.println("Hello World friends");
 		}
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
