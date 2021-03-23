03c03de6c6fc671f718ad9fc88f02e25435cc578
NandiniUpadhyay <nandiniupadhyay1902@gmail.com>
2021-03-23 11:23:43 +0530

    added master1fn3

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -41,5 +41,8 @@ public class Calculator {
 	public static void masterfn2(int num1){
 	      System.out.println(num1*num1*num1*num1*num1);
 	  }
+	public static void masterfn3(int num1){
+	      System.out.println(num1*num1*num1*num1*num1);
+	  }
 	
 }
