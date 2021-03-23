5cd7ea4222d723e0f37d45de90d66203d35d98ea
Admin <Admin@DESKTOP-B5465N8>
2021-03-23 11:07:20 +0530

    master1fn2 added

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -39,5 +39,8 @@ public class Calculator {
 	public static void masterfn1(int num1){
 	      System.out.println(num1*num1*num1*num1*num1);
 	  }
+	public static void masterfn2(int num1){
+	      System.out.println(num1*num1*num1*num1*num1);
+	  }
 	
 }
